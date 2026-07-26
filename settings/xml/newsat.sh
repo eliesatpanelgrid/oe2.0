7aea7d413380e6b6dbc160aed98644be38eec362
eliesatpanelgrid <eliesatpanelgrid@gmail.com>
2026-07-26 12:03:59 +0300

    Add files via upload

--- settings/xml/newsat.sh
+++ settings/xml/newsat.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-file_path="/etc/enigma2/satellites.xml"
+file_path="/etc/tuxbox/satellites.xml"
+
 echo "> Downloading & Installing Satellites Frequencies File Please Wait..."
 sleep 3
 
-if [ -f $file_path ]; then
-echo "> Removing file old version please wait..."
-sleep 3 
-rm -rf $file_path > /dev/null 2>&1
+if [ -f "$file_path" ]; then
+    echo "> Removing old version please wait..."
+    sleep 3
+    rm -f "$file_path" > /dev/null 2>&1
 fi
 
-wget --show-progress -qO /etc/tuxbox/satellites.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/newsat.xml
+wget --show-progress -qO /etc/tuxbox/satellites.xml https://raw.githubusercontent.com/eliesatpanelgrid/oe2.0/main/settings/xml/newsat.xml
 
 echo
-    echo "> satellite.xml file is installed successfully"
-    echo "> Maintained By ElieSatpanelgrid team"
-    echo
-    sleep 2
+echo "> satellites.xml file installed successfully"
+echo "> Maintained By ElieSatpanelgrid team"
+echo
+sleep 2
