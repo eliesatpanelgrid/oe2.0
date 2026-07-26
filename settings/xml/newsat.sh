1e9c49e8c0aac50f58b7261c1b3bbb146a003518
eliesatpanelgrid <eliesatpanelgrid@gmail.com>
2026-07-26 12:31:24 +0300

    Add files via upload

--- settings/xml/newsat.sh
+++ settings/xml/newsat.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 file_path="/etc/tuxbox/satellites.xml"
 url="https://raw.githubusercontent.com/eliesatpanelgrid/oe2.0/main/settings/xml/newsat.xml"
+file_path2="/etc/enigma2/satellites.xml"
+if [ -f $file_path2 ]; then
+echo "> Removing file old version please wait..."
+sleep 3 
+rm -rf $file_path2 > /dev/null 2>&1
+fi
 
 echo "> Downloading & Installing Satellites Frequencies File Please Wait..."
 sleep 2
 
-# Use curl if available (handles modern GitHub SSL better on Enigma2)
 if command -v wget >/dev/null 2>&1; then
    wget --show-progress -qO --no-check-certificate "$file_path" "$url"
 else
-    # Fallback to wget without quiet mode to reveal errors
     curl -L -k -o "$file_path" "$url"
 fi
 
-# Check if file actually downloaded and isn't empty
 if [ -s "$file_path" ]; then
     echo
     echo "> satellites.xml file is installed successfully"
