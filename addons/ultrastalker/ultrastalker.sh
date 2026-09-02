e9a514ed759367b6c33036452a31ce0c308e4fc1
eliesatpanelgrid <eliesatpanelgrid@gmail.com>
2026-09-02 18:39:41 +0300

    Add files via upload

--- addons/ultrastalker/ultrastalker.sh
+++ addons/ultrastalker/ultrastalker.sh
@@ -18,37 +18,38 @@ temp_dir="/tmp"
 # Determine package manager
 #########################################
 if command -v dpkg &> /dev/null; then
-package_manager="apt"
-status_file="/var/lib/dpkg/status"
-uninstall_command="apt-get purge --auto-remove -y"
+    package_manager="apt"
+    status_file="/var/lib/dpkg/status"
+    uninstall_command="apt-get purge --auto-remove -y"
 else
-package_manager="opkg"
-status_file="/var/lib/opkg/status"
-uninstall_command="opkg remove --force-depends"
+    package_manager="opkg"
+    status_file="/var/lib/opkg/status"
+    uninstall_command="opkg remove --force-depends"
 fi
 
 #check and_remove package old version
 #########################################
 check_and_remove_package() {
-if [ -d $plugin_path ]; then
-echo "> removing package old version please wait..."
-sleep 3 
-rm -rf $plugin_path > /dev/null 2>&1
+    if [ -d $plugin_path ]; then
+        echo "> removing package old version please wait..."
+        sleep 3
+        rm -rf $plugin_path > /dev/null 2>&1
 
-if grep -q "$package" "$status_file"; then
-echo "> Removing existing $package package, please wait..."
-$uninstall_command $package > /dev/null 2>&1
-fi
-echo "*******************************************"
-echo "*        Removal Completed Successfully   *"
-echo "*            Maintained by Eliesat        *"
-echo "*******************************************"
-sleep 3
-echo
-exit 1
-else
-echo " " 
-fi  }
+        if grep -q "$package" "$status_file"; then
+            echo "> Removing existing $package package, please wait..."
+            $uninstall_command $package > /dev/null 2>&1
+        fi
+        echo "*******************************************"
+        echo "*        Removal Completed Successfully   *"
+        echo "*            Maintained by Eliesat        *"
+        echo "*******************************************"
+        sleep 3
+        echo
+        exit 1
+    else
+        echo " "
+    fi
+}
 check_and_remove_package
 
 #download & install dependencies
@@ -98,57 +99,58 @@ is_installed() {
 if [ -z "$DEPS" ]; then
     :
 else
+    echo "Updating package lists..."
+    $PM_UPDATE >/dev/null 2>&1
+    echo ""
 
-echo "Updating package lists..."
-$PM_UPDATE >/dev/null 2>&1
-echo ""
-
-for pkg in $DEPS; do
-    if is_installed "$pkg"; then
-        echo "[OK] $pkg already installed"
-    else
-        echo "[INSTALL] $pkg"
-        if $PM_INSTALL $pkg >/dev/null 2>&1; then
-            echo "[DONE] $pkg"
+    for pkg in $DEPS; do
+        if is_installed "$pkg"; then
+            echo "[OK] $pkg already installed"
         else
-            echo "[FAIL] $pkg"
+            echo "[INSTALL] $pkg"
+            if $PM_INSTALL $pkg >/dev/null 2>&1; then
+                echo "[DONE] $pkg"
+            else
+                echo "[FAIL] $pkg"
+            fi
         fi
-    fi
-done
-
+    done
 fi
 
 #download & install package
 #########################################
 print_message() {
-echo "> [$(date +'%Y-%m-%d')] $1"
+    echo "> [$(date +'%Y-%m-%d')] $1"
 }
-download_and_install_package() {
-print_message "> Downloading $plugin-$version package  please wait ..."
-sleep 3
-wget --show-progress -qO $temp_dir/$targz_file --no-check-certificate $url
-tar -xzf $temp_dir/$targz_file -C / > /dev/null 2>&1
-extract=$?
-rm -rf $temp_dir/$targz_file >/dev/null 2>&1
 
-if [ $extract -eq 0 ]; then
-
-set -e
-rm -rf /media/hdd/UltraStalker/cast 2>/dev/null || true
-if [ -d /media/hdd/UltraStalker/library ]; then
-    find /media/hdd/UltraStalker/library -type d -name cast -prune -exec rm -rf '{}' + 2>/dev/null || true
-
-  print_message "> $plugin-$version package installed successfully"
 cleanup() {
-[ -d "/CONTROL" ] && rm -rf /CONTROL >/dev/null 2>&1
-rm -rf /control /postinst /preinst /prerm /postrm /tmp/*.ipk /tmp/*.tar.gz >/dev/null 2>&1
+    [ -d "/CONTROL" ] && rm -rf /CONTROL >/dev/null 2>&1
+    rm -rf /control /postinst /preinst /prerm /postrm /tmp/*.ipk /tmp/*.tar.gz >/dev/null 2>&1
 }
-cleanup
-print_message "> Maintained By ElieSatpanelgrid team"
-echo
-sleep 3
-else
-  print_message "> $plugin-$version package download failed"
-  sleep 3
-fi  }
+
+download_and_install_package() {
+    print_message "> Downloading $plugin-$version package please wait ..."
+    sleep 3
+    wget --show-progress -qO $temp_dir/$targz_file --no-check-certificate $url
+    tar -xzf $temp_dir/$targz_file -C / > /dev/null 2>&1
+    extract=$?
+    rm -rf $temp_dir/$targz_file >/dev/null 2>&1
+
+    if [ $extract -eq 0 ]; then
+        set -e
+        rm -rf /media/hdd/UltraStalker/cast 2>/dev/null || true
+        if [ -d /media/hdd/UltraStalker/library ]; then
+            find /media/hdd/UltraStalker/library -type d -name cast -prune -exec rm -rf '{}' + 2>/dev/null || true
+        fi
+
+        print_message "> $plugin-$version package installed successfully"
+        cleanup
+        print_message "> Maintained By ElieSatpanelgrid team"
+        echo
+        sleep 3
+    else
+        print_message "> $plugin-$version package download failed"
+        sleep 3
+    fi
+}
 download_and_install_package
